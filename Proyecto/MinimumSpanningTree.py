0d0431fefa3304dadc6da5091d1437b6e031d0e3
Ricardo J. Castillo López <87798579+rjcastillolopez@users.noreply.github.com>
2022-02-05 18:16:48 -0500

    update segmentation, mst

--- Proyecto/MinimumSpanningTree.py
+++ Proyecto/MinimumSpanningTree.py
@@ -1,109 +1,82 @@
 import math
+from queue import Queue
 from segmentation import *
-import queue
 
 class MinimumSpanningTree(object):
-    # Parameters
-    # ----------
-    # labels : dictionary, key = label, value = list of bounding in order y1, y2, x1, x2
-    #
-    # Fields
-    # ----------
-    # l_weight : a list of tuple (tuple[0]: a tuple of vertex, tuple[1]: weight)
+
     def __init__(self, labels):
         self.d_mst = {}
         l_weight = []
+        # Calcular los pesos entre todos los nodos (componentes etiquetados)
         for i in labels.keys():
             for j in labels.keys():
-                # l_weight.append([i, j])
                 if j > i:
                     weight = self.calculate_weight(labels[i], labels[j])
-                    t = [[i, j], weight]
+                    t = [[i, j], weight] # Peso entre dos nodos
                     l_weight.append(t)
-        self.l_weight = sorted(l_weight, key=lambda x: x[1])
+        self.l_weight = sorted(l_weight, key=lambda x: x[1]) # Ordena los pesos ascendentemente
+        #print(self.l_weight)
         self.build_tree()
 
-    # Return
-    # ----------
-    # dictionary :  key = vertex, value = a list of tuples (tuple: [connected vertex, weight])
     def get_mst(self):
         return self.d_mst
 
-    # Parameters
-    # ----------
-    # labels : dictionary, key = label, value = list of bounding in order y1, y2, x1, x2
-    #
-    # Return
-    # ----------
-    # number : distance of the strokes
-    @staticmethod
-    def calculate_weight(l_bounding_1, l_bounding_2):
+    def calculate_weight(self, l_bounding_1, l_bounding_2):
+        '''
+        Calcula la distancia (pesos) entre los cuadros delimitadores de dos componentes etiquetados
+        :params l_bounding_1, l_bounding_2: Diccionario. key = etiqueta del componente, values: lista de coordenadas del cuadro delimitador
+        :return: Distancia entre los dos nodos (componentes)
+        '''
         y_mean_1 = (l_bounding_1[0] + l_bounding_1[1]) / 2.0
         y_mean_2 = (l_bounding_2[0] + l_bounding_2[1]) / 2.0
         x_mean_1 = (l_bounding_1[2] + l_bounding_1[3]) / 2.0
         x_mean_2 = (l_bounding_2[2] + l_bounding_2[3]) / 2.0
+        # Calcular distancia euclidinana
         euclidean_distance = math.sqrt(math.pow(x_mean_1 - x_mean_2, 2) + math.pow(y_mean_1 - y_mean_2, 2))
         return euclidean_distance
 
-
-    # Description
-    # ----------
-    # build the MinimumSpanningTree
     def build_tree(self):
+        '''
+        Devuelve la estructura del árbol de expansión mínimo (Minimum Spanning Tree)
+        :return: Estructura de árbol
+        '''
         d_mst = self.d_mst
-
         while self.l_weight:
             edge = self.l_weight.pop(0)
-            # print 'edge', edge
-            vertex1 = edge[0][0]
-            vertex2 = edge[0][1]
-            # print d_mst
-
-            if self.is_disconnected(vertex1, vertex2):
-                self.dictionary_add(d_mst, vertex1, [vertex2, edge[1]])
-                self.dictionary_add(d_mst, vertex2, [vertex1, edge[1]])
-                # print 'add: ', vertex1, ',', vertex2, '\t', vertex2, ',', vertex1, '\n'
-
-        # print d_mst
+            v1 = edge[0][0] # Primer nodo
+            v2 = edge[0][1] # Segundo nodo
+            #print(d_mst)
+            if self.is_disconnected(v1, v2): # Los nodos no están conectados
+                # Vincular al primer nodo el nodo no conectado y su peso
+                self.dictionary_add(d_mst, v1, [v2, edge[1]]) 
+                self.dictionary_add(d_mst, v2, [v1, edge[1]])
         return d_mst
 
-
-
-    # Parameters
-    # ----------
-    # vertex1 : int
-    # vertex2 : int
-    #
-    # Return
-    # ----------
-    # boolean : True if vertex1 and vertex2 are disconnected
-    def is_disconnected(self, vertex1, vertex2):
+    def is_disconnected(self, v1, v2):
+        '''
+        Verifica si dos nodos no están conectados (no son conexos)
+        :param v1, v2: nodos
+        :return: True si los dos nodos no están conectados y False caso contrario
+        '''
         d_mst = self.d_mst
-        if (vertex1 not in d_mst.keys()) or (vertex2 not in d_mst.keys()):
+        if (v1 not in d_mst.keys()) or (v2 not in d_mst.keys()):
             return True
-
-        if vertex1 in self.d_mst.keys():
-            return self.search(vertex1, vertex2)
+        if v1 in self.d_mst.keys():
+            return self.search(v1, v2)
         else:
-            return self.search(vertex2, vertex1)
+            return self.search(v2, v1)
 
-
-    # Parameters
-    # ----------
-    # v1 : int
-    # v2 : int
-    #
-    # Return
-    # ----------
-    # boolean : True if v2 is among all the vertex that is reachable for v1
     def search(self, v1, v2):
-        l_visited = []
-        q = queue.Queue()
+        '''
+        Verifica si el nodo v2 se encuentra conectado con uno de los nodos que están conectados al nodo v1
+        :param v1, v2: nodos
+        '''
+        l_visited = [] # Nodos visitados
+        q = Queue()
         q.put(v1)
         while q.qsize() > 0:
             cur_v = q.get()
             l_visited.append(cur_v)
-
             for t in self.d_mst[cur_v]:
                 neighbour = t[0]
                 if v2 == neighbour:
@@ -112,20 +85,73 @@ class MinimumSpanningTree(object):
                     q.put(neighbour)
         return True
 
-    @staticmethod
-    def dictionary_add(d, key, t):
+    def dictionary_add(self, d, key, t):
+        '''
+        # Vincula un nodo del árbol de expansión mínimo con otro nodo no conectado
+        :param Key: primer nodo
+        :param t: Array. Segundo nodo vinculado y la distancia o peso al primer nodo
+        '''
         if key not in d.keys():
             d[key] = []
         if t not in d[key]:
             d[key].append(t)
+    
+    def center_of_mass(self, img, coords):
+        '''
+        Dada una imagen, retorna el centro de masa de la imagen       
+        :param img: La imagen que será procesada
+        :param coords: [y_min, y_max, x_min, x_max]
+        :return: El centro de masa el objeto en la imagen
+        '''
+        sum_x = coords[2]
+        sum_y = coords[0]
+        k = 0 # [y_min, y_max, x_min, x_max]
+        for i in range(coords[2], coords[3]):
+            for j in range(coords[0], coords[1]):
+                if img[j][i] > 0:
+                    sum_x += i*img[j][i]
+                    sum_y += j*img[j][i]
+                    k += img[j][i]
+
+        sum_x = sum_x // k
+        sum_y = sum_y // k
+        return sum_x, sum_y
+    
+    def draw_MST(self, img, labels):
+        '''
+        Devuelve la estructura del MST en la imagen original
+        :param: img: Imagen original en escala de grises 
+        :param: labels: Dict. Componentes etiquetados
+        :param: width: Ancho de la linea del árbol
+        :return: Imagen original con los cuadros delimitadores de todas los componentes etiquetados
+        '''
+        origin = color.gray2rgb(img)
+        for v in self.d_mst.keys():
+            coords_v = labels[v]
+            center_v = self.center_of_mass(img, coords_v)
+            for v_con in self.d_mst[v]:
+                coords_v_con = labels[v_con[0]] 
+                center_v_con = self.center_of_mass(img, coords_v_con)
+                cv2.line(origin, (center_v[0], center_v[1]), (center_v_con[0], center_v_con[1]), (0, 255, 0))
+                #self.draw_line(origin, red, center_v[0], center_v[1], center_v_con[0], center_v_con[1])
+
+        return origin
 
 if __name__ == '__main__':
     # d = {1: [21, 150, 434, 533], 2: [26, 79, 683, 775], 3: [43, 66, 489, 523], 4: [47, 74, 735, 779], 5: [76, 86, 479, 535],
     #      6: [84, 91, 600, 625], 7: [88, 99, 678, 788], 8: [96, 104, 596, 618], 9: [98, 136, 495, 523],
     #      10: [117, 166, 683, 778], 11: [135, 171, 739, 769]}
 
-    fname = './equations2/SKMBT_36317040717260_eq6.png'
-    seg = Segmentation(fname)
+    import imageio
+    import cv2
+    # Leer la imagen
+    img = cv2.imread('SKMBT_36317040717260_eq16.png', cv2.IMREAD_GRAYSCALE)
+    # Binarizar la imagen
+    ret2, th2 = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    # Segmentar la imagen
+    seg = Segmentation(th2)
     d = seg.get_labels()
     mst = MinimumSpanningTree(d)
-    print(mst.get_mst())
+    img_mst = mst.draw_MST(th2, d)
+    imageio.imsave('img_mst.png', img_mst.astype(np.uint8))
+
--- Proyecto/segmentation.py
+++ Proyecto/segmentation.py
@@ -1,44 +1,38 @@
 import scipy
 from scipy import ndimage
-from scipy import misc
 import numpy as np
 import sys
-import datetime
+from skimage import color
 
 class Segmentation(object):
 
-    # Parameters
-    # ----------
-    # img_path : string
-    def __init__(self, path):
-        blur_radius = 1.0
-        threshold = 50
-
-        img = misc.imread(path)
+    def __init__(self, img):
         self.origin = img
-        # suavizar la imagen (para eliminar objetos pequeños)
+        # Aplicar filtro gaussiano para eliminar ruido y suavizar la imagen
+        blur_radius = 1.0
         imgf = ndimage.gaussian_filter(img, blur_radius)
-        threshold = 50
-
-        # buscar componentes conectados
-        self.img, self.count = ndimage.label(imgf > threshold)
-        #print(imgf > threshold)
+        # Encontrar componentes (número de caracteristicas)
+        # labeled_img es una imagen etiquetada de acuerdo a num_features
+        threshold = 100
+        self.labeled_img, self.num_features = ndimage.label(imgf > threshold)
+        # Obtener las coordenadas del cuadro delimitador para cada componente etiquetado
         self.labels = self.calculate_labels()
 
-
-    # Return
-    # ----------
-    # labels : dictionary, key = label, value = list of bounding in order y1, y2, x1, x2
     def get_labels(self):
         return self.labels
-
-
+    
     def calculate_labels(self):
+        '''
+        Devuelve las coordenadas del cuadro delimitador para cada componente etiquetado de la imagen.    
+        :return: Diccionario de etiquetas para cada componente y las coordenadas de su cuadro delimitador.
+                 Coordenadas: [y_min, y_max, x_min, x_max]
+        '''
         labels = {}
-        for label in range(1, self.count + 1):
-            labels[label] = [sys.maxsize, -1, sys.maxsize, -1]
+        for label in range(1, self.num_features + 1):
+            labels[label] = [sys.maxsize, -1, sys.maxsize, -1] # [y_min, y_max, x_min, x_max]
 
-        img = self.img
+        # Calcular las coordenadas del cuadro delimitador para cada componente
+        img = self.labeled_img
         for row in range(img.shape[0]):
             for col in range(img.shape[1]):
                 value = img[row][col]
@@ -52,7 +46,8 @@ class Segmentation(object):
                         bounding[1] = row
                     if bounding[3] < col:
                         bounding[3] = col
-
+                        
+        # Eliminar los componentes donde el área del cuadro delimitador es menor a 50
         to_del = []
         for label in labels:
             bounding = labels[label]
@@ -60,93 +55,64 @@ class Segmentation(object):
                 to_del.append(label)
         for key in to_del:
             del labels[key]
+            
         return labels
 
-
-    # Parameters
-    # ----------
-    # label : int
-    #     Label of the stroke to be found
-    #
-    # Returns
-    # -------
-    # np.ndarray
-    # the stroke image in format of np array
     def get_stroke(self,label):
-        l = self.labels[label]
-
-        stroke = np.copy(self.img[l[0]:l[1] + 1,l[2]:l[3] + 1])
-
-        """
-        following is binary stroke
-        """
-        # for data in np.nditer(stroke, op_flags=['readwrite']):
-        #     if data != label:
-        #         data[...] = 0
-        #     else:
-        #         data[...] = 255
-
-        """
-        following is the stroke from origin image
-        """
+        '''
+        Devuelve el trazo de un componente etiquetado     
+        :param label: Diccionario. key = etiqueta del componente, values: lista de coordenadas del cuadro delimitador
+        :return: Trazo del componente.
+        '''
+        # Obtener las coordenadas del cuadro delimitador
+        l = self.labels[label] # [y_min, y_max, x_min, x_max]
+        # Obtener el trazo a partir de las coordenadas del cuadro delimitador en la imagen etiquetada
+        y_min = l[0]
+        y_max = l[1]
+        x_min = l[2]
+        x_max = l[3]
+        stroke = np.copy(self.labeled_img[y_min:y_max + 1, x_min:x_max + 1])
         shape = stroke.shape
         for i in range(shape[0]):
             for j in range(shape[1]):
                 if stroke[i][j] != label:
-                    stroke[i][j] = 0
+                    stroke[i][j] = 0 # Eliminar componentes que se traslapan dentro del cuadro delimitador
                 else:
-                    stroke[i][j] = self.origin[i + l[0]][j + l[2]]
-
+                    stroke[i][j] = self.origin[i+y_min][j+x_min] # Obtener pixeles originales
+        
         return stroke
 
-
-    # Parameters
-    # ----------
-    # l_labels : list
-    #     A list of stroke labels representing the strokes to be combined
-    #
-    # Returns
-    # -------
-    # np.ndarray
-    # the combined stroke image in format of np array
     def get_combined_strokes(self,l_labels):
-        bounding = self.get_combined_bounding(l_labels)
-        stroke = np.copy(self.img[bounding[0]:bounding[1] + 1, bounding[2]:bounding[3] + 1])
-
-        """
-        following is binary stroke
-        """
-        # for data in np.nditer(stroke, op_flags=['readwrite']):
-        #     if data in l_labels:
-        #         data[...] = 255
-        #     else:
-        #         data[...] = 0
-
-        """
-        following is the stroke from origin image
-        """
+        '''
+        Devuelve el conjunto de componentes etiquetados en un único trazo
+        :param l_labels: Diccionario del conjunto de componentes etiquetados
+        :return: Trazo único del conunto de componentes etiquetados
+        '''
+        # Obtener las coordenadas del cuadro delimitador del conjunto total de componentes
+        bounding = self.get_combined_bounding(l_labels) # [y_min, y_max, x_min, x_max]
+        # Obtener el trazo a partir de las coordenadas del cuadro delimitador en la imagen etiquetada
+        y_min = bounding[0]
+        y_max = bounding[1]
+        x_min = bounding[2]
+        x_max = bounding[3]
+        stroke = np.copy(self.labeled_img[y_min:y_max + 1, x_min:x_max + 1])
         shape = stroke.shape
         for i in range(shape[0]):
             for j in range(shape[1]):
                 if stroke[i][j] in l_labels:
-                    stroke[i][j] = self.origin[i + bounding[0]][j + bounding[2]]
+                    stroke[i][j] = self.origin[i+y_min][j+x_min] # Obtener pixeles originales
                 else:
                     stroke[i][j] = 0
 
         return stroke
 
-
-    # Parameters
-    # ----------
-    # l_labels : list
-    #     A list of stroke labels representing the strokes to be combined
-    #
-    # Returns
-    # -------
-    # list
-    # the list of bounding of combined strokes
     def get_combined_bounding(self,l_labels):
-        l = [sys.maxsize, -1, sys.maxsize, -1]
+        '''
+        Devuelve las coordenadas del cuadro delimitador de un conjunto de componentes etiquetados
+        :param l_labels: Diccionario del conjunto de componentes etiquetados
+        :return: Cuadro delimitador del conjunto de componentes etiquetados
+        '''
+        l = [sys.maxsize,-1,sys.maxsize,-1] # Coordenadas del cuadro delimitador 
         for label in l_labels:
             bounding = self.labels[label]
             if bounding[0] < l[0]:
@@ -157,8 +123,26 @@ class Segmentation(object):
                 l[2] = bounding[2]
             if bounding[3] > l[3]:
                 l[3] = bounding[3]
+
         return l
 
+    def draw_boundings(self, width):
+        '''
+        Devuelve el cuadro delimitador de todos los componentes en la imagen original
+        :param width: Ancho de la linea del cuadro delimitador
+        :return: Imagen original con los cuadros delimitadores de todas los componentes etiquetados
+        '''
+        comp_boxes = color.gray2rgb(self.origin)
+        green = np.array([0, 255, 0], dtype=np.uint8)
+        w = width # ancho de la linea
+        for label in self.labels.keys(): 
+            coord = self.labels[label] # Coordenadas del cuadro delimitador de cada componente
+            comp_boxes[coord[0] - w:coord[0], coord[2] - w:coord[3] + w] = green
+            comp_boxes[coord[1]:coord[1] + w, coord[2] - w:coord[3] + w] = green
+            comp_boxes[coord[0]:coord[1], coord[2] - w:coord[2]] = green
+            comp_boxes[coord[0]:coord[1], coord[3]:coord[3] + w] = green
+        
+        return comp_boxes
 
 if __name__ == '__main__':
     fname = 'annotated/SKMBT_36317040717280_eq3.png'
@@ -171,10 +155,6 @@ if __name__ == '__main__':
         stroke = seg.get_stroke(label)
         scipy.misc.imsave("./tmp/"+ str(label) + ".png", stroke)
 
-    #
     origin = seg.origin
     scipy.misc.imsave('./tmp/origin.png', origin)
 
-
-    # combined = seg.get_combined_strokes([1,2])
-    # scipy.misc.imsave('./tmp/combined.png', combined)
