8e07fe95c316dba7ee95362238642132466aca1c
Ricardo J. Castillo López <87798579+rjcastillolopez@users.noreply.github.com>
2022-02-05 19:18:40 -0500

    Update partition

--- Proyecto/partition.py
+++ Proyecto/partition.py
@@ -1,35 +1,25 @@
-# TODO
-# input mst
-# ----------
-# dictionary :  key = vertex, value = a list of tuples (tuple: [connected vertex, weight])
-# output lst
-# ----------
-# lists :  list of format [symbol, y1, y2, x1, x2, list of labels]
 from segmentation import Segmentation
 from MinimumSpanningTree import MinimumSpanningTree
 from collections import deque, defaultdict
-import imghdr  # recognize img type
+import imghdr 
 from os import listdir, getcwd, sep
 from os.path import isfile, join
 from subprocess import call
-import re
 import json
 import tensorflow as tf
 from scipy import misc
 import numpy as np
 from MER_NN import SymbolRecognition
 from skimage.morphology import binary_dilation,dilation,disk
-import scipy
-from PIL import Image
 
+# Leer el archivo JSON del conjunto de simbolos y guardar el contenido en un diccionario
 symMap = {}
-#abre el archivo Symbol_mapping en formato json 
 with open('symbol_mapping.json', 'r') as opened:
-    #lee el archivo Symbol_mapping y los guarda en symMap
-    symMap = json.loads(opened.read())
+        symMap = json.loads(opened.read())
 print(symMap)
 
 class Partition(object):
+    
     def __init__(self, mst, seg, sess, sr):
         self.mst = mst
         self.seg = seg
@@ -38,48 +28,56 @@ class Partition(object):
         self.lst = []
         self.generateList()
         self.count = defaultdict(lambda:0)
-    #retorna la lista
+
     def getList(self):
         return self.lst
 
     def calculateCount(self):
+        '''
+        Calcula la cantidad de elementos únicos
+        '''
         for e in self.lst:
             self.count[e[0]]+=1
-    #retorna la cantidad
+
     def getCount(self):
+        '''
+        Devuelve la cantidad de items generados
+        '''
         return self.count
 
     def generateList(self):
+        '''
+        Devuelve una lista con los simbolos más probables que contiene la expresión. 
+        '''
         generated = []
         dots = []
         visited = set([])
-        #verifica si la longitud de la lista es igual a 0
+        # Verificar si el MST no tiene elementos y termina.
         if len(self.mst)==0:
             return
-        #
+        # Recorrer todos los simbolos del MST y almacenar en una lista. 
         for e in self.mst:
             queue = deque([e])
             break
+
         while len(queue)>0:
             v = queue.popleft()
             visited.add(v)
-            image = self.seg.get_combined_strokes([v])
-            bb = self.seg.get_combined_bounding([v])
+            image = self.seg.get_combined_strokes([v]) # Combinar trazos
+            bb = self.seg.get_combined_bounding([v]) # Obtener las coordenadas del cuadro delimitador de los trazos combinados
             image = self.input_wrapper_arr(image)
-            # test = self.sr.pr(image)
             p = self.sr.p(image)
-            # probability = self.sess.run(tf.nn.softmax(test)[0][0][0][p[0]])
             p = symMap[str(p[0])]
-            # print probability,p
-            # if probability>0. :
             self.lst.append([p,bb[0],bb[1],bb[2],bb[3],[v]])
             generated.append(v)
+            '''
+            Para el símbolo “.”: Si existen 2 símbolos “.” y existe un símbolo “-” entre ellos, combinar estos tres elementos, crear
+            un nuevo símbolo denominado “div” y eliminar los 3 elementos anteriores. Si existen consecutivamente 3 símbolos “.”, crear
+            un nuevo símbolo denominado “dots”.
+            '''
             if p=="-":
-                # print p,bb
                 pass
-            #se define la multiplicacion
             elif p=="dot":
-                # print "dot case"
                 self.lst.pop()
                 dots.append(v)
                 if len(dots)>1 and len(self.lst)>0 and self.lst[-1][0]=="-":
@@ -89,10 +87,7 @@ class Partition(object):
                     image = self.input_wrapper_arr(image)
                     test = self.sr.pr(image)
                     p = self.sr.p(image)
-                    # probability = self.sess.run(tf.nn.softmax(test)[0][0][0][p[0]])
                     p = symMap[str(p[0])]
-                    # print probability,l,p
-                    # if probability>0.:
                     self.lst.pop()
                     self.lst.append(["div",bb[0],bb[1],bb[2],bb[3],l])
                     dots.pop()
@@ -103,13 +98,14 @@ class Partition(object):
                     image = self.input_wrapper_arr(image)
                     test = self.sr.pr(image)
                     p = self.sr.p(image)
-                    # probability = self.sess.run(tf.nn.softmax(test)[0][0][0][p[0]])
                     p = symMap[str(p[0])]
-                    # print probability,dots,p
-                    # if probability>0.:
                     self.lst.append(["dots",bb[0],bb[1],bb[2],bb[3],dots])
                     dots = []
-            #se define que es una multiplicacion
+            
+            '''
+            Si a la izquierda y derecha del símbolo “x” existen variables o el símbolo “frac”, entonces se reconoce como una 
+            multiplicación “mul”
+            '''
             elif p=="x" and len(self.lst)>1 and self.lst[-2][0] in ["a","b","c","d","frac"]:
                 self.lst[-1][0]="mul"
 
@@ -118,8 +114,8 @@ class Partition(object):
                     continue
                 queue.append(w[0])
 
+        # Ordenar la lista de elementos
         self.lst.sort(key = lambda x : x[3])
-        # print self.lst
         le = len(self.lst)
         centerList = []
         minusList = []
@@ -127,21 +123,27 @@ class Partition(object):
         tList = []
         deleteList = []
         idx = 0
-        #recorremos la lista  
+
+        # Recorrer la lista para reconocer los simbolos "-", "s" y "t"
         for e in self.lst:
-            # guardamos en la lista centerList guardamos los datos apartir de la indice 1
             centerList.append([(e[1]+e[2])/2,(e[3]+e[4])/2])
-            #si en la pocicion 0 es igual a "-" guardamos en la lista minusList
+            #si en la posición 0 es igual a "-" guardamos en la lista minusList
             if e[0]=="-":
                 minusList.append(idx)
-            #si en la pocicion 0 es igual a "s" guardamos en la lista sList
+            #si en la posición 0 es igual a "s" guardamos en la lista sList
             elif e[0]=="s":
                 sList.append(idx)
-            #si en la pocicion 0 es igual a "t" guardamos en la lista tList
+            #si en la posición 0 es igual a "t" guardamos en la lista tList
             elif e[0]=="t":
                 tList.append(idx)
             idx+=1
-        # recorremos en la minusList esta comineza con "-" en el cual definiremos si es una fracion  una igauldad
+        
+        '''
+        Para el símbolo “-”: Si existen 2 símbolos “-” donde las coordenadas en x del cuadro delimitador de ambos tienen un rango 
+        en común, entonces se reconocen como el símbolo “=”. Si existe el símbolo “+” sobre el símbolo “-”, ambos se reconocen como 
+        el símbolo “pm” (plus minus para abreviar). Si existen símbolos encima y debajo de “-”, se trata de una fracción, entonces 
+        se reconoce como “frac”. Si solo existe un elemento debajo de “-”, se reconoce como el símbolo de barra horizontal “bar”.
+        '''
         for i in minusList:
             if i in deleteList:
                 continue
@@ -186,23 +188,26 @@ class Partition(object):
                     self.lst[i][0] = "frac"
                 elif down>0:
                     self.lst[i][0] = "bar"
-        # recorremos en la sList esta comineza con "s" en el cual definiremos si es una funcion coseno o seno
+  
+        '''
+        Para los símbolos “sin”, “cos”: Estos símbolos pueden no estar conectados en el MST. Se recorre la lista ordenada
+        de símbolos para obtener su índice de ubicación. Para el símbolo “s” pueden existir 2 casos: que “s” pertenezca a “cos” o 
+        que pertenezca a “sin”,en cualquier caso, dependerá de la ubicación del símbolo.
+        '''
         for i in sList:
             if i in deleteList:
                 continue
             k=i-1
-            #la funcion coseno si k=i-1 la pocicion actual se encuentra "s", en k se encuentra "o"  y en k-1 se encuentra "c" se trata de la funcion coseno
+            # la funcion coseno si k=i-1 la pocicion actual se encuentra "s", en k se encuentra "o"  y en k-1 se encuentra "c" se trata de la funcion coseno
             if k>0 and self.lst[k][4]<self.lst[k+1][3] and self.lst[k][0]=="o" and self.lst[k-1][0]=="c":
-                # to the left of i
                 deleteList.append(i)
                 deleteList.append(i-1)
                 deleteList.append(i-2)
                 l = self.lst[i][5]+self.lst[i-1][5]+self.lst[i-2][5]
                 bb = self.seg.get_combined_bounding(l)
                 self.lst.append(["cos",bb[0],bb[1],bb[2],bb[3],l])
-            #la funcion seno si en la pocion actual mas 3 encontramos a n es la funcion seno
+            # la funcion seno si en la pocion actual mas 3 encontramos a n es la funcion seno
             elif i+3<le and self.lst[i+3][0]=="n":
-                # right
                 deleteList.append(i)
                 deleteList.append(i+1)
                 deleteList.append(i+2)
@@ -210,7 +215,8 @@ class Partition(object):
                 l = self.lst[i][5]+self.lst[i+1][5]+self.lst[i+2][5]+self.lst[i+3][5]
                 bb = self.seg.get_combined_bounding(l)
                 self.lst.append(["sin",bb[0],bb[1],bb[2],bb[3],l])
-        # recorremos en la tList esta comineza con "t" en el cual definiremos si es una funcion tagente
+
+        # Para el simbolo "tan", se recorre la lista para determinar la ubicación de los simbolos "a" y "n"
         for i in  tList:
             if i in deleteList:
                 continue
@@ -222,32 +228,31 @@ class Partition(object):
                 bb = self.seg.get_combined_bounding(l)
                 self.lst.append(["tan",bb[0],bb[1],bb[2],bb[3],l])
 
-        dL = sorted(deleteList,reverse=True)
+        dL = sorted(deleteList, reverse=True)
         for e in dL:
             del self.lst[e]
 
-    def input_wrapper_arr(self,image):
-        #obtenemos las dimenciones de la imagen
+    def input_wrapper_arr(self,image):   
+        '''
+        Devuelve la imagen procesada aplicando padding (rellenar con ceros), operación morfológica de dilatación y 
+        redimensión del tamaño a 32x32
+        '''  
         sx,sy = image.shape
-        #obtenemos la diferencia de las dimenciones de la imagen 
         diff = np.abs(sx-sy)
-        sx,sy = image.shape
-        
+        # Rellenar la imagen con ceros para igualar las dimensiones
         image = np.pad(image,((sx//8,sx//8),(sy//8,sy//8)),'constant')
         if sx > sy:
             image = np.pad(image,((0,0),(diff//2,diff//2)),'constant')
         else:
             image = np.pad(image,((diff//2,diff//2),(0,0)),'constant')
-        #hace un dilatacion a la imagen
-        image = dilation(image)#, footprint= disk(max(sx,sy)/32))
-        #image = np.array(Image.fromarray(image).resize((32,32), Image.BILINEAR))
-        #redimenciona la imagen
+        # Aplicar operación morfológica de dilatación
+        image = dilation(image)
+        # Resimensionar la imagen a 32x32
         image = misc.imresize(image,(32,32))
-        #binariza la imagen 
+        # Binarizar la imagen:
         if np.max(image) > 1:
             image = image/255.
         return image
-
 
 if __name__ == '__main__':
     model_path = join(getcwd(), "model", "model.ckpt")
